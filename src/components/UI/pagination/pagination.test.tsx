fe226ba0eeb7af0afe4221f5289225f5d94f235f
Artem Zhuravlev <an3wer@yandex.ru>
2023-10-05 08:24:23 +0500

    added some tests for pagination

--- src/components/UI/pagination/pagination.test.tsx
+++ src/components/UI/pagination/pagination.test.tsx
@@ -1,35 +1,63 @@
 /* eslint-disable testing-library/no-render-in-setup */
 import { render, screen } from "@testing-library/react";
 import Pagintaion from "./Pagination";
-
-// const mockOnPage = (p: number) => {
-//   console.log(p);
-// };
+import userEvent from "@testing-library/user-event";
 
 const mockProps = {
   currentPage: 1,
   pageCount: 10,
-  onPage(p: number) {},
 };
 
+const onPage = jest.fn();
+
 describe("Pagination", () => {
-  beforeEach(() =>
+  test("should be render in DOM", () => {
     render(
       <Pagintaion
         current={mockProps.currentPage}
         pageCount={mockProps.pageCount}
-        onPage={mockProps.onPage}
+        onPage={onPage}
       />
-    )
-  );
+    );
 
-  test("should be render in DOM", () => {
     const getPagination = screen.getByRole("navigation");
+
     expect(getPagination).toBeInTheDocument();
   });
 
   test("should be render all page buttons", () => {
+    render(
+      <Pagintaion
+        current={mockProps.currentPage}
+        pageCount={mockProps.pageCount}
+        onPage={onPage}
+      />
+    );
     const getButtons = screen.getAllByRole("button");
     expect(getButtons.length).toBe(mockProps.pageCount + 2);
+  });
+
+  test("onPage should be called", () => {
+    render(
+      <Pagintaion
+        current={mockProps.currentPage}
+        pageCount={mockProps.pageCount}
+        onPage={onPage}
+      />
+    );
+    userEvent.click(screen.getByText("2"));
+    expect(onPage).toHaveBeenCalled();
+  });
+
+  test("Pagination snapshot", () => {
+    const view = render(
+      <Pagintaion
+        current={mockProps.currentPage}
+        pageCount={mockProps.pageCount}
+        onPage={onPage}
+      />
+    );
+
+    expect(view).toMatchSnapshot();
   });
 });
